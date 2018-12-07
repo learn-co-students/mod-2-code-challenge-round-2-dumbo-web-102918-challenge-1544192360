3ea1e42d43d28832b7b475e0cda8f723fe514e67
Brian Horowitz <guitarrook94@gmail.com>
2018-12-07 10:46:17 -0500

    Crushed code challenge

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,7 @@
 class Episode < ApplicationRecord
+  has_many :appearances
+  has_many :guests, :through => :appearances
 end
+
+
+#A guest can have many appearances, and an episode can have many appearances. An appearance belongs to a guest, and an episode.
